0671f095ae1423a379837c594b02b8d01bf0926e
Samuel Bourke <sam@bourke-uk.com>
2021-02-26 18:29:09 +0000

    Improved table customisation

--- pytools/display/ColumnDefinition.py
+++ pytools/display/ColumnDefinition.py
@@ -1,7 +1,8 @@
 class ColumnDefinition:
-	def __init__(self, header, header_style=None, cell_style=None, cell_style_context=None):
+	def __init__(self, header, header_style=None, cell_style=None, cell_style_context=None, justify="left"):
 		self.header = header
 		self.header_style = header_style
 		self.cell_style = cell_style
 		self.cell_style_context = cell_style_context
+		self.justify = justify
 
--- pytools/display/__init__.py
+++ pytools/display/__init__.py
@@ -5,17 +5,19 @@ from rich.table import Table
 
 from pytools.display.ColumnDefinition import ColumnDefinition
 
+__default_cell_style = "[cyan]"
+__null_cell_style = "[bright_black]<null>"
+
 
 def table(headers, rows, title=None):
-	table = Table(header_style="b orange3", title=title, box=box.ROUNDED, border_style="b blue")
+	table = Table(title=title, box=box.ROUNDED, border_style="b blue")
 	for column in headers:
 		if not isinstance(column, ColumnDefinition):
-			table.add_column(column, style="cyan")
+			table.add_column(column, justify="left")
 		else:
-			print(column.cell_style)
 			table.add_column(column.header)
 	for row in rows:
-		table.add_row(*map(str, __format_row(list(row), headers)))
+		table.add_row(*map(str, __format_row(list(row), headers)), )
 	return table
 
 
@@ -30,8 +32,12 @@ def __format_row(row, columns):
 
 
 def __format_value(value, column):
+	if value is None:
+		return __null_cell_style
 	if not isinstance(column, ColumnDefinition) or column.cell_style is None:
 		return value
+	if column.cell_style == "standard":
+		return f"[{column.cell_style_context}]{value}"
 	if column.cell_style == "pos_nev_num":
 		return __apply_pos_nev_num(value)
 	if column.cell_style == "conditional":
@@ -41,13 +47,13 @@ def __format_value(value, column):
 
 def __apply_pos_nev_num(value):
 	if value > 0:
-		return "[green]" + str(value)
+		return f"[green]{value}"
 	if value < 0:
-		return "[red]" + str(value)
-	return "[cyan]" + str(value)
+		return f"[red]{value}"
+	return __default_cell_style + str(value)
 
 
 def __apply_conditional(value, context):
 	if str(value) in context:
-		return "[" + context[str(value)] + "]" + str(value)
-	return "[cyan]" + str(value)
+		return f"[{context[str(value)]}]{value}"
+	return __default_cell_style + str(value)
